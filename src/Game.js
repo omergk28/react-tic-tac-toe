7ad40c6ff24d3a603fd8062cd56b6ec34473835a
omergk <omergk28@gmail.com>
2023-11-26 11:01:34 -0500

    add some tests, WIP

--- src/Game.js
+++ src/Game.js
@@ -1,13 +1,11 @@
 import {useState} from "react";
 
-function Square({value, onSquareClicked}) {
-  return (
-      <>
-        <button onClick={onSquareClicked} className="square">
+function Square({value, onSquareClicked, index}) {
+    return (<>
+        <button onClick={onSquareClicked} className="square" data-testid={`square-${index}`}>
             {value}
         </button>
-      </>
-  );
+    </>);
 }
 
 function Board({player, squares, onPlay}) {
@@ -18,38 +16,33 @@ function Board({player, squares, onPlay}) {
         const newSquares = squares.slice();
         newSquares[index] = player;
         onPlay(newSquares);
-    };
+    }
 
     const winner = calculateWinner(squares);
     let status;
     if (winner) {
         status = "Winner is " + winner;
-    }
-    else {
+    } else {
         status = "Next player is " + player;
     }
 
-    function getRow(i) {
-        const rows = [0,1,2]
-            .map(r=> r+(i*3))
-            .map(r=> <Square value={squares[r]} onSquareClicked={() => handleClick(r)}/>);
-        return rows;
+    function BoardRow({rowNumber}) {
+        return <>
+            <div className="board-row" data-testid={'row'+rowNumber}>
+                {[0, 1, 2]
+                    .map(col => col + (rowNumber * 3))
+                    .map(col => <Square key={'column' + col} value={squares[col]} index={col}
+                                        onSquareClicked={() => handleClick(col)}/>)
+                }
+            </div>
+        </>
     }
 
-  return (
-      <>
+    return (<>
         <div className="status">{status}</div>
-        <div className="board-row">
-            {getRow(1)}
-        </div>
-        <div className="board-row">
-            {getRow(2)}
-        </div>
-        <div className="board-row">
-            {getRow(3)}
-        </div>
-      </>
-  );
+        {[0, 1, 2].map(i => <BoardRow key={'row' + i} rowNumber={i}/>)})
+    </>);
+
 }
 
 
@@ -76,43 +69,29 @@ export default function Game() {
         let description;
         if (move > 0) {
             description = "Go to move # " + move;
-        }
-        else {
+        } else {
             description = "Go to game start";
         }
-        return (
-            <li key={move}>
-                <button onClick={() => jumpTo(move)}>{description}</button>
-            </li>
-        );
+        return (<li key={'move' + move}>
+            <button key={'jump' + move} onClick={() => jumpTo(move)}>{description}</button>
+        </li>);
     });
 
-    return (
-        <div className="game">
-            <div className="game-board">
-                <Board player={player} squares={currentSquares} onPlay={handlePlay} />
-            </div>
-            <div className="game-info">
-                <ol>{moves}</ol>
-            </div>
+    return (<div className="game" data-testid="game">
+        <div className="game-board">
+            <Board player={player} squares={currentSquares} onPlay={handlePlay}/>
         </div>
-    );
+        <div className="game-info">
+            <ol>{moves}</ol>
+        </div>
+    </div>);
 }
 
 function calculateWinner(squares) {
-    const lines = [
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        [0,4,8],
-        [2,4,6]
-    ];
+    const lines = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 
-    for (let i=0; i< lines.length; i++) {
-        const [a,b,c] = lines[i];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
         if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
             return squares[a];
         }
--- src/Game.test.js
+++ src/Game.test.js
@@ -1,9 +1,21 @@
-import { render, screen } from '@testing-library/react';
-import Game from './Game';
+import {render, screen} from '@testing-library/react';
+import Game from '../src/Game';
 
 test('renders Game', () => {
-  render(<Game />);
-  const linkEyeaylement = screen.getAllByTestId();
-  const linkElement2 = screen.getByRole();
-  expect(linkElement).toBeInTheDocument();
+    render(<Game/>);
+    const game = screen.getByTestId('game');
+    expect(game).toBeInTheDocument();
+});
+
+test('shows board rows and clickable squares', () => {
+    render(<Game/>);
+    for (const i of [...Array(9).slice(1).keys()]) {
+        const squareAtIndex = screen.getByTestId(`square-${i}`);
+        expect(squareAtIndex).toBeInTheDocument();
+    }
+
+    for (const i of Array(3).keys()) {
+        const rowAtIndex = screen.getByTestId(`row${i}`);
+        expect(rowAtIndex).toBeInTheDocument();
+    }
 });
